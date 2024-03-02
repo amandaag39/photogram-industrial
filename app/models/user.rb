31028512c5a9459c4dfc9bf0238e2b78024c7d48
Jason Johnson <145226961+JJohnson606@users.noreply.github.com>
2024-03-02 19:15:31 +0000

    Generated Accessors

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,4 +29,20 @@ class User < ApplicationRecord
 
          has_many :own_photos, class_name: "Photo", foreign_key: "owner_id"
          has_many :comments, foreign_key: "author_id"
+  
+         has_many :sent_follow_requests, foreign_key: :sender_id, class_name: "FollowRequest"
+         has_many :accepted_sent_follow_request, -> {where(status: "accepted")}, foreign_key: :sender_id, class_name: "FollowRequest"
+
+         has_many :recieved_follow_requests, foreign_key: :recipient_id, class_name: "FollowRequest"
+         has_many :accepted_received_follow_requests, -> { where(status: "accepted") }, foreign_key: :recipient_id, class_name: "FollowRequest"
+
+         has_many :liked_photos, through: :likes, source: :photo
+
+         has_many :leaders, through: :accepted_sent_follow_request, source: :recipient
+         has_many :followers, through: :accepted_received_follow_requests, source: :recipient
+
+         has_many :feed, through: :leaders, source: :own_photos
+         
+         has_many :discover, through: :leaders, source: :liked_photos
+
 end
